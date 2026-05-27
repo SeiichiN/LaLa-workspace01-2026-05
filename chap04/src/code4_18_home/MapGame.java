f837722a17faf5c502f7b995bd06853f6c04f5ee
SeiichiN <billie175@gmail.com>
2026-05-28 08:06:16 +0900

    MapGame added.

--- chap04/src/code4_18_home/MapGame.java
+++ chap04/src/code4_18_home/MapGame.java
@@ -12,8 +12,17 @@ public class MapGame {
 		};
 		int y = 1;
 		int x = 2;
+		System.out.print("現在位置 [" + y + ":" + x + "] ");
 		System.out.println(map[y][x]);
-		
+		String ch = new java.util.Scanner(System.in).nextLine();
+		switch (ch) {
+		case "w" -> { y--; }
+		case "a" -> { x--; }
+		case "s" -> { y++; }
+		case "d" -> { x++; }
+		}
+		System.out.print("現在位置 [" + y + ":" + x + "] ");
+		System.out.println(map[y][x]);
 	}
 
 }
