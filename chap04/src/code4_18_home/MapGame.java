7f4178945f9595fdd28342d656b72b63e918ad5a
SeiichiN <billie175@gmail.com>
2026-05-27 21:23:50 +0900

    map added.

---
+++ chap04/src/code4_18_home/MapGame.java
@@ -0,0 +1,19 @@
+package code4_18_home;
+
+public class MapGame {
+
+	public static void main(String[] args) {
+		String[][] map = {
+				{".", ".", ".", "p", "."},
+				{".", ".", "g", ".", "."},
+				{".", ".", ".", ".", "e"},
+				{".", "s", ".", ".", "."},
+				{".", ".", ".", ".", "."},
+		};
+		int y = 1;
+		int x = 2;
+		System.out.println(map[y][x]);
+		
+	}
+
+}
