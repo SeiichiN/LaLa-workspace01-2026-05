ea3d558a247297549b4248612c9b1d6990208878
SeiichiN <billie175@gmail.com>
2026-05-28 16:25:19 +0900

    5/28 p207まで

---
+++ chap04/src/code4_19/Main.java
@@ -0,0 +1,21 @@
+package code4_19;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int[][] scores = {
+				{40, 50, 60},
+				{80, 60, 70}
+		};
+		System.out.println(scores.length);
+		System.out.println(scores[0].length);
+		
+		for (int[] y : scores) {
+			for (int x : y) {
+				System.out.println(x);
+			}
+		}
+			
+	}
+
+}
---
+++ chap05/src/code5_12/Main.java
@@ -0,0 +1,18 @@
+package code5_12;
+
+public class Main {
+	
+	public static void printArray(int[] array) {
+		array[0] = 100;
+		for (int element : array) {
+			System.out.println(element);
+		}
+	}
+
+	public static void main(String[] args) {
+		int[] array = {1, 2, 3};
+		printArray(array);
+		System.out.println(array[0]); // 100
+	}
+
+}
